6e8fd0a79e19e6e5231e4ce7d01963d2d1796bf8
tkardozo <t.cardoso@ua.pt>
2016-05-28 19:52:45 +0100

    Add files via upload

--- App/main.js
+++ App/main.js
@@ -43,16 +43,18 @@ function checkLogin(){
 
 function getCookie(cname) {
     var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
-    }
+	if(document.cookie!=null){
+		var ca = document.cookie.split(';');
+		for(var i = 0; i <ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length,c.length);
+			}
+		}
+	}
     return "";
 	
 }
